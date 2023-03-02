c6702b018ad7c0e856a84119b5ebbcf334e4842d
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-03-02 13:23:11 +0530

    document

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        ``
         <script src="https://code.iconify.design/iconify-icon/1.0.3/iconify-icon.min.js"></script>
+        <link rel="shortcut icon" href="/logoPrimary.png" type="image/x-icon" />
       </Head>
       <body>
         <Main />
